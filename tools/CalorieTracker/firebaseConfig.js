63247c6a9e23acb045f6f6f5c65c0ff6b56348d8
ferynd <67190638+ferynd@users.noreply.github.com>
2025-07-18 10:27:56 -0500

    Delete tools/CalorieTracker directory

--- tools/CalorieTracker/firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyBHdodVoGI962K1MWeyAp519duJX22ZDss",
-  authDomain: "jlb-calorietracker.firebaseapp.com",
-  projectId: "jlb-calorietracker",
-  storageBucket: "jlb-calorietracker.firebasestorage.app",
-  messagingSenderId: "994744135946",
-  appId: "1:994744135946:web:ccf2aa11ce73c2c77c4af3",
-  measurementId: "G-3SWQ8KQ2EQ"
-};
